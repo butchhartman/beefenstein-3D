f83c4737a899d94d22cb3deb4dd8c5661e420b7e
butchhartman <rcamacho5436@gmail.com>
2025-03-05 22:05:30 -0700

    Forgot to stage. Same as last commit.

---
+++ include/SDLbase.h
@@ -0,0 +1,19 @@
+﻿// GPbase.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+// Ensures GLFW won't include the development environment header. This prevents conflicts upon inclusion of gl3w .
+#define SDL_MAIN_USE_CALLBACKS 1 // uses callbacks instead of the main as an entry point
+
+#include <stdio.h>
+
+// Window creation library. Also handles other things like input and audio.
+#include "../vendored/SDL3/include/SDL3/SDL.h"
+#include "../vendored/SDL3/include/SDL3/SDL_main.h"
+
+
+
+#define DEFAULT_WIDTH 640
+#define DEFAULT_HEIGHT 480
+
+ 
